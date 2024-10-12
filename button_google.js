c36f231cc6790ef5adce78d5720db5b14cc29395
skylarp14 <pine0061@algonquinlive.com>
2024-10-12 03:51:50 -0400

    Replaced current geoAPI link into a google api link

---
+++ button_google.js
@@ -0,0 +1,40 @@
+// geoAPI Link is from google api
+
+const http = new XMLHttpRequest()
+let result = document.querySelector("#result")
+
+document.querySelector("#share").addEventListener("click", () => {
+    findMyCoordinates()
+})
+
+function findMyCoordinates(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+            (position) =>{
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                
+                console.log("Latitude: ", latitude, "Longitude: ", longitude);
+                
+                const geoAPI = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyDwrOSN1lzpjga-RY3zVGYs9mUWSWp6VJ4`;
+                
+                getAPI(geoAPI);
+            },
+            (err) => {
+                alert(err.message);
+            }
+        )
+    } else{
+        alert("Geolocation unavailable in your current browser!");
+    }
+}
+
+function getAPI(geoAPI){
+    http.open("GET", geoAPI);
+    http.send();
+    http.onreadystatechange = function(){
+        if(this.readyState === 4 && this.status === 200){
+            result.innerHTML = this.responseText;
+        }
+    }
+}
